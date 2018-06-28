ce9e1d37a581e609853aa2a788790b379392727d
AndreasMN <andycutter@godmail.dk>
2018-06-28 10:58:17 +0200

    renamed main-css to fizzbuzz css. Changed IDs to fit fizzbuzz and be less generic

--- source/javascripts/fizz-buzz-display.js
+++ source/javascripts/fizz-buzz-display.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let button = document.getElementById('button')
-    let displayDiv = document.getElementById('display_answer')
+    let button = document.getElementById('fizz_button')
+    let displayDiv = document.getElementById('display_fizzbuzz')
     button.addEventListener('click', () => {
-        let value = document.getElementById('value').value
+        let value = document.getElementById('fizz_value').value
         let fizzBuzz = new FizzBuzz
         let result = fizzBuzz.check(value)
         displayDiv.innerHTML = result;
